55b44a46498dbeea16a54d7c9baff7dc2f994828
lakshayt7 <lt2504@bigpurple-ln3.cm.cluster>
2023-12-01 19:16:26 -0500

    adding requirements and code changes

--- image_eval.sh
+++ image_eval.sh
@@ -2,7 +2,7 @@
 #SBATCH --nodes=1
 #SBATCH --tasks-per-node=2
 #SBATCH --cpus-per-task=4
-#SBATCH --time=24:00:00
+#SBATCH --time=12:00:00
 #SBATCH --mem-per-cpu=16G
 #SBATCH --output=logs/exp_jobid_%j.out
 #SBATCH --error=logs/image-eval%j.err
@@ -16,4 +16,4 @@ source /gpfs/scratch/lt2504/image-eval/imagen/bin/activate
 
 python -m pytorch_fid $1 $2
   
-python image-eval/Image-Generation-Evaluation/src/main.py --gen_image_path $1 --gt_images $2 --text_prompt "photo of class <sks> 
+python src/main.py --gen_image_path $1 --gt_images $2 --text_prompt "photo of class <sks>"
--- src/clip_score.py
+++ src/clip_score.py
@@ -2,6 +2,52 @@ import clip
 import torch
 from torchvision import transforms
 
+import os
+from PIL import Image
+
+def load_and_convert_images(directory_path):
+    # Check if the directory exists
+    if not os.path.exists(directory_path):
+        print(f"The directory {directory_path} does not exist.")
+        return
+
+    # Get a list of all files in the directory
+    image_files = [f for f in os.listdir(directory_path) if os.path.isfile(os.path.join(directory_path, f))]
+
+    # Check if there are any image files in the directory
+    if not image_files:
+        print(f"No image files found in {directory_path}.")
+        return
+
+    # Define a transformation to convert the images to tensors
+    transform = transforms.ToTensor()
+
+    # Load and convert each image to a torch tensor
+    image_tensors = []
+    
+    for image_file in image_files:
+        image_path = os.path.join(directory_path, image_file)
+        
+        try:
+            # Open the image using PIL
+            img = Image.open(image_path)
+
+            # Apply the transformation to convert the image to a torch tensor
+            
+            img_tensor = transform(img).unsqueeze(0)
+
+            # Print information about the converted image
+            #print(f"Image: {image_file}, Size: {img.size}, Tensor Shape: {img_tensor.shape}")
+            image_tensors.append(img_tensor)
+            
+        
+        except Exception as e:
+            print(f"Error processing {image_file}: {e}")
+            
+    all_samples = torch.cat(image_tensors, axis=0)
+    
+    return all_samples
+
 class CLIPEvaluator(object):
     def __init__(self, device, clip_model='ViT-B/32') -> None:
         self.device = device
@@ -61,8 +107,15 @@ class ImageDirEvaluator(CLIPEvaluator):
         super().__init__(device, clip_model)
 
     def evaluate(self, gen_samples, src_images, target_text):
+        
+        gen_images_pts = load_and_convert_images(gen_samples)
+        src_images_pts = load_and_convert_images(gen_samples)
+        
+        print(gen_images_pts.shape)
+        print(src_images_pts.shape)
+        
+        sim_samples_to_img  = self.img_to_img_similarity(src_images_pts, gen_images_pts)
 
-        sim_samples_to_img  = self.img_to_img_similarity(src_images, gen_samples)
-        sim_samples_to_text = self.txt_to_img_similarity(target_text.replace("*", ""), gen_samples)
+        sim_samples_to_text = self.txt_to_img_similarity(target_text.replace("*", ""), gen_images_pts)
 
         return sim_samples_to_img, sim_samples_to_text
